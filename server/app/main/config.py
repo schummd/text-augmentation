ed692cf605bf09d58a7efff3736814ba5145bf12
Alexander Catic <caticalexander@gmail.com>
2021-07-03 20:52:14 +0300

    Updated server boilerplate

--- server/app/main/config.py
+++ server/app/main/config.py
@@ -1,5 +1,8 @@
 import os
 
+# uncomment the line below for postgres database url from environment variable
+# postgres_local_base = os.environ['DATABASE_URL']
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -12,6 +15,8 @@ class Config:
 
 
 class DevelopmentConfig(Config):
+    # uncomment the line below to use postgres
+    # SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -27,6 +32,8 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
+    # uncomment the line below to use postgres
+    # SQLALCHEMY_DATABASE_URI = postgres_local_base
 
 
 config_by_name = dict(
