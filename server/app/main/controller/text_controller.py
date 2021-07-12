edf4f4562fbabd0d008011f37d33e01ac6df1ba3
Sophie <levsophie@gmail.com>
2021-07-11 21:19:23 -0700

    retrieving specific text for specific user

--- server/app/main/controller/text_controller.py
+++ server/app/main/controller/text_controller.py
@@ -1,27 +1,53 @@
 from flask import request
-from flask_restx import Resource
+from flask_restx import Resource, fields
 from flask_restx.reqparse import RequestParser
-
+import flask
 from app.main.util.decorator import token_required
-from ..util.dto import TextDto
-from ..service.text_service import save_new_text
+from ..util.dto import TextDto, UserDto
+from ..service.text_service import save_new_text, retrieve_text
 from typing import Dict
 
 api = TextDto.api
 _text = TextDto.text
+_user = UserDto.user
 
 
-@api.route('/')
+
+@api.route('/', methods=['POST'])
 class Text(Resource):
     @api.expect(_text, validate=True)
     @token_required
     @api.response(201, 'Text successfully saved.')
-    @api.doc('create a new user')
+    @api.doc('create a new text')
     @api.header("hello", "test")
     def post(self) -> Dict[str, str]:
         """Saves a new text"""
         data = request.json
         return save_new_text(data=data)
 
+
+'''Retrieves specific text of the indicated user'''
+@api.response(200, 'OK')
+@api.response(400, 'Bad request')
+@api.route('/<string:username>/<string:text_id>', methods=['GET'])
+class UserSpecificText(Resource): 
+    
+    @token_required
+    @api.response(201, 'Text found.')
+    @api.doc('retrieve text')
+    @api.marshal_with(_text)
+    def get(self, username, text_id) -> Dict[str, str]:
+        """Retrieves text from db"""
+        data = request.json
+        try:
+            res =  retrieve_text(username, text_id, data=data)
+        except:
+            print("Didn't work...")
+        title = res.text_title
+        body = res.text_body
+        resp = {'text_title': title, 'text_body': body}
+        print(resp)
+        return resp
+       
 
 
--- server/app/main/service/text_service.py
+++ server/app/main/service/text_service.py
@@ -2,9 +2,11 @@ import uuid
 import datetime
 
 from flask.globals import request
+from flask import abort
 
 from app.main import db
 from app.main.model.text import Text
+from app.main.model.user import User
 from app.main.service.auth_helper import Auth
 
 
@@ -23,15 +25,30 @@ def save_new_text(data: Dict[str, str]) -> Dict[str, str]:
         user_id=logged_in_user['user_id'],
         text_body=data['text_body'],
     )
-    
     save_changes(new_text)
     
     response_object = {
            'status': 'success',
            'message': 'Successfully added text.'
        }
-    
     return response_object
+
+'''Retrieves specific text of the indicated user'''
+def retrieve_text(username, text_id, data: Dict[str, str]) -> Dict[str, str]:
+    logged_in_user = Auth.get_logged_in_user(request)[0]['data']
+    user_id = logged_in_user['user_id']
+    # check if the user requesting the text is connected to the logged in user
+    
+    print(text_id)
+    try:
+        requestedText = Text.query.join(User, Text.user_id==User.id).\
+        filter(User.username==username).\
+        filter(Text.text_id==text_id).first()
+        print(requestedText)
+        return requestedText    
+    except:
+        return abort (404, "Bad request, either user or text not found")
+    
 
 def save_changes(data: Text) -> None:
     db.session.add(data)
