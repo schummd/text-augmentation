64b1a976acdc2a8d6f56fcb3790c0efa9295deef
Daria Schumm <zonamoroza@gmail.com>
2021-07-12 14:17:28 +1000

    added GET, PUT, DELETE for /text/{username}/{text_id}

--- server/app/main/model/text.py
+++ server/app/main/model/text.py
@@ -6,12 +6,11 @@ class Text(db.Model):
     """ Text Model for storing text related details """
     __tablename__ = "text"
 
+    user_id = db.Column(db.String(50), unique=False)
     text_id = db.Column(db.String(50), primary_key=True)
     created_on = db.Column(db.DateTime, nullable=False)
     text_title = db.Column(db.String(100), unique=False)
-    user_id = db.Column(db.String(50), unique=False)
     text_body = db.Column(db.Text)
-
 
     def __repr__(self):
         return f'<Text {self.text_id}>'
--- server/app/main/service/text_service.py
+++ server/app/main/service/text_service.py
@@ -2,7 +2,8 @@ import uuid
 import datetime
 
 from flask.globals import request
-from sqlalchemy.orm import load_only
+from sqlalchemy.orm import load_only, session
+from werkzeug.datastructures import UpdateDictMixin
 
 from app.main import db
 from app.main.model.text import Text
@@ -22,10 +23,10 @@ def save_new_text(username, data: Dict[str, str]) -> Dict[str, str]:
     # print(logged_in_user)
     
     new_text = Text(
+        user_id=user.id,
         text_id=str(uuid.uuid4()),
         created_on=datetime.datetime.utcnow(),
         text_title=data['text_title'],
-        user_id=user.id,
         text_body=data['text_body'],
     )
     
@@ -38,10 +39,51 @@ def save_new_text(username, data: Dict[str, str]) -> Dict[str, str]:
     
     return response_object
 
+
 def get_all_texts(username):
     return Text.query.join(User, Text.user_id==User.id).filter(User.username==username).all()
+
+
+def get_a_text(username, text_id): 
+    return Text.query.join(User, Text.user_id==User.id).filter(User.username==username, Text.text_id==text_id).first() 
+
+
+def update_text(username, text_id, data: Dict[str, str]) -> Dict[str, str]: 
+    # get user_id from provided username 
+    user = User.query.filter_by(username=username).first()
+    # get row to update 
+    update = Text.query.filter_by(text_id=text_id, user_id=user.id).first() 
+    # update text title and body
+    update.text_title = data['text_title']  # update created_on time too? 
+    update.text_body = data['text_body']
+
+    db.session.commit() 
+
+    response_object = {
+           'status': 'success',
+           'message': 'Successfully updated text.'
+       }
+    
+    return response_object
+
+
+def delete_a_text(username, text_id):
+    # get user_id from provided username 
+    user = User.query.filter_by(username=username).first() 
+    # get row to delete 
+    row = Text.query.filter_by(text_id=text_id, user_id=user.id).first() 
+
+    db.session.delete(row)
+    db.session.commit()
+
+    response_object = {
+           'status': 'success',
+           'message': 'Successfully deleted text.'
+       }
+    
+    return response_object
+
 
 def save_changes(data: Text) -> None:
     db.session.add(data)
     db.session.commit()
-
